a826e4145ed8cb528f9c96fa433a6e148a22241d
shuaile <18736380266@163.com>
2017-04-12 01:03:53 +0800

    SLLIU

---
+++ ios-Note/ios-Note.h
@@ -0,0 +1,29 @@
+//
+//  ios-Note.h
+//  ios-Note
+//
+//  Created by SL on 12/04/2017.
+//  Copyright © 2017 Sam. All rights reserved.
+//
+
+#ifndef ios_Note_h
+#define ios_Note_h
+
+/*
+ 
+ 1、测量启动时间
+ #MARK: Environment Variable
+ Xcode -> Edit scheme -> Run -> Auguments 将环境变量 DYLD_PRINT_STATISTICS设为1
+ 
+ 2、代码静态分析
+ #MARK: Static Analyzer - Analysis Policy
+ Xcode -> Build Settings -> Analyzer During 'Build' 设定为YES
+ 
+ 3、僵尸指针追踪
+ #MARK: Memory Management
+ Xcode -> Edit scheme -> Run -> Diagnostics 勾选Zombie Objects
+ 
+ 
+ */
+
+#endif /* ios_Note_h */
