32b407330b0bbcbf5d4ff1eeeb754d428b095917
shuaile <18736380266@163.com>
2017-04-11 23:26:54 +0800

    LiuShuaile

---
+++ ios-Note/Note/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Note
+//
+//  Created by SL on 3/24/17.
+//  Copyright © 2017 Sam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
