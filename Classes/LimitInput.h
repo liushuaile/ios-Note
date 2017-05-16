90f7c6e2783f64dc25ca8170de364086c216ddfd
shuaile <18736380266@163.com>
2017-05-16 20:23:53 +0800

    commit file of SLlibs.podspec

---
+++ Classes/LimitInput.h
@@ -0,0 +1,25 @@
+//
+//  LimitInput.h
+//  singleview
+//
+
+#import <Foundation/Foundation.h>
+#import <objc/runtime.h>
+
+
+#define PROPERTY_NAME @"limit"
+
+#define DECLARE_PROPERTY(className) \
+@interface className (Limit) @end
+
+DECLARE_PROPERTY(UITextField)
+DECLARE_PROPERTY(UITextView)
+
+@interface LimitInput : NSObject
+
+@property(nonatomic, assign) BOOL enableLimitCount;
+
++(LimitInput *) sharedInstance;
+
+
+@end
